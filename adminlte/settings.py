2bede5243ca699cd3289bdd2ecf1c81393f33c59
JONATHAN ATONG <110544038+atongjonathan@users.noreply.github.com>
2024-02-27 00:13:37 +0300

    fix: req

--- adminlte/settings.py
+++ adminlte/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-l)7c*j3ydy4f16s8jc54wrv=57-8v=c!oavk8u57qjp&jtl8td'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["127.0.0.1", "atongjona.pythonanywhere.com", "ngong.itsfixed.africa", "www.ngong.itsfixed.africa", "portal.itsfixed.africa"]
 
