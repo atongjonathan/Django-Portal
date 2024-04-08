5cb64c4767152c92859db354548bcd4fd7654bfd
JONATHAN ATONG <110544038+atongjonathan@users.noreply.github.com>
2024-04-08 21:15:46 +0300

    pep8

--- portal/statement_data.py
+++ portal/statement_data.py
@@ -13,13 +13,10 @@ def combine_data():
     return combined_df.drop_duplicates()
 
 
-def get_statements(id, year=None):
+def get_statements(id):
     combined = combine_data()
-    student = combined.loc[combined["Customer No__x"]
-                           == id].copy()  # Make a copy of the DataFrame
-
-    # student_2023 = student[~student['Posting Date_x'].str.contains("2023")] if year is not None else student[student['Posting Date_x'].str.contains("2023")]
-    student_2023 = student
+    student_2023 = combined.loc[combined["Customer No__x"]
+                                == id].copy()  # Make a copy of the DataFrame
     student_2023.drop(columns=["Posting Date_y", "Customer No__y",
                                "Customer No__x", "Amount", "Entry No_"], inplace=True)
     student_2023.rename(columns={'Posting Date_x': 'date', 'Description': 'particular',
@@ -27,7 +24,6 @@ def get_statements(id, year=None):
     student_2023["date"] = pd.to_datetime(student_2023["date"])
     df_sorted = student_2023.sort_values(by="date")
     df_sorted['date'] = df_sorted['date'].dt.strftime('%d/%m/%y')
-
     return format_data(df_sorted)
 
 
@@ -48,6 +44,3 @@ def format_data(student_2023):
             row["credit"], ',.2f')
         row["bal"] = format(bal, ',.2f')
     return student_2023
-
-
-# print(get_statements("STU-0227")[1])
