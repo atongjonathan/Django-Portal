db3d4047ab8ce04a2580888602c701e64ec9a398
JONATHAN ATONG <110544038+atongjonathan@users.noreply.github.com>
2024-04-12 00:19:57 +0300

    fix: month

--- portal/static/portal/custom/js/statement.js
+++ portal/static/portal/custom/js/statement.js
@@ -3,4 +3,4 @@ var currentDate = new Date();
 var day = currentDate.getDate();
 var month = currentDate.getMonth();
 var year = currentDate.getFullYear();
-document.getElementById("date").innerText = `${day}/${month}/${year}`;
+document.getElementById("date").innerText = `${day}/${month+1}/${year}`;
